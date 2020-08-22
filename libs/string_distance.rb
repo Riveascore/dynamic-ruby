2e239f455be78e5e1e98c69857d2d7d1c33cfdcc
Chris Aga <chrisaga.dev@gmail.com>
2020-08-21 22:27:06 -0500

    Set up string distance

---
+++ libs/string_distance.rb
@@ -0,0 +1,26 @@
+# string edit distance
+# Levenshtein distance
+
+# can be used for autocomplete
+
+# similarity caculated by
+# how many steps to convert one string to another
+
+# delete letter
+# add letter
+# replace letter
+
+class StringDistance
+  def initialize(variable)
+    @variable = variable
+  end
+
+  def method_1
+    
+  end
+
+  def method_2
+    
+  end
+  
+end
---
+++ test/string_distance_test.rb
@@ -0,0 +1,21 @@
+require_relative "../libs/string_distance.rb"
+require "test/unit"
+require "minitest/autorun"
+
+class StringDistanceTest < Minitest::Test
+  def setup
+    
+  end
+  
+  def test_1
+
+  end
+  
+  def test_2
+    
+  end
+
+  def test_3
+    
+  end
+end
