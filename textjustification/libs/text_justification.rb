85aee4d261f10cbf3ede4d644aa42db81a448abd
Chris Aga <chrisaga.dev@gmail.com>
2020-08-21 20:23:20 -0500

    Get format working

--- textjustification/libs/text_justification.rb
+++ textjustification/libs/text_justification.rb
@@ -13,9 +13,9 @@ class TextJustification
   def count_char(from, to)
     char_count = 0
 
-    for i in from..to
+    for i in from..(to-1)
       char_count += @map[i].size
-      char_count += 1 if i < to
+      char_count += 1 if i < (to-1)
     end
 
     char_count
@@ -30,17 +30,17 @@ class TextJustification
   end
 
   def format(n)
-    end_val = @map.size - 1
+    end_val = @map.size
 
-    if n > end_val
+    if n == end_val
       return 0
     end
 
     min_score = Float::INFINITY
-    for i in n..end_val
+    for i in (n + 1)..end_val
       count_val = count_char(n, i)
       ug = ugliness(count_val)
-      formatted = format(n + 1)
+      formatted = format(i)
       score = ug + formatted
 
       min_score = [
