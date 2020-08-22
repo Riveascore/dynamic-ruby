8296111a4ba18ac0caea76785b8600ebd5183a4b
Chris Aga <chrisaga.dev@gmail.com>
2020-08-21 19:19:05 -0500

    Set up textjustification

---
+++ textjustification/libs/text_justification.rb
@@ -0,0 +1,14 @@
+class TextJustification
+  def initialize(variable)
+    @variable = variable
+  end
+
+  def method_1
+    
+  end
+
+  def method_2
+    
+  end
+  
+end
---
+++ textjustification/test/text_justification_test.rb
@@ -0,0 +1,21 @@
+require_relative "../libs/text_justification.rb"
+require "test/unit"
+require "minitest/autorun"
+
+class TextJustificationTest < Minitest::Test
+  def setup
+    
+  end
+  
+  def test_1
+
+  end
+  
+  def test_2
+    
+  end
+
+  def test_3
+    
+  end
+end
