15a8f7b5dac08959af6bd310263121f33e800bab
Chris Aga <chrisaga.dev@gmail.com>
2020-08-22 23:01:49 -0500

    Setup new methods

--- libs/full_bus_tour.rb
+++ libs/full_bus_tour.rb
@@ -6,34 +6,50 @@ class FullBusTour
     @full_cap = full_cap
   end
 
-  def sum_array(start, ending)
-    value_at_start = @group_sizes[start]
-    if value_at_start == @full_cap
-      @answer = value_at_start
-    end
+  # def sum_array(start, ending)
+  #   value_at_start = @group_sizes[start]
+  #   if value_at_start == @full_cap
+  #     @answer = value_at_start
+  #   end
     
-    solution = if start == ending
-      value_at_start
-    else
-      value_at_start + sum_array(start, ending - 1)
-    end
+  #   solution = if start == ending
+  #     value_at_start
+  #   else
+  #     value_at_start + sum_array(start, ending - 1)
+  #   end
 
-    if solution == @full_cap
-      @answer = solution
-    end
+  #   if solution == @full_cap
+  #     @answer = solution
+  #   end
 
-    solution
+  #   solution
+  # end
+
+  def check_all_nodes
+    
   end
+
+  def tree_sum(n)
+    
+  end
+
+  def is_correct(val)
+    
+  end
+  
+  
+  
   
 
   def fits_exactly(length = @group_sizes.length, c = @full_cap)
-    last_index = @group_sizes.size - 1
-    for i in 0..last_index
-      sum_array(i, last_index)
-    end
+    # last_index = @group_sizes.size - 1
+    # for i in 0..last_index
+    #   sum_array(i, last_index)
+    # end
 
-    puts "@group_sizes: #{@group_sizes}"
-    puts "@answer: #{@answer}"
+    # puts "@group_sizes: #{@group_sizes}"
+    # puts "@answer: #{@answer}"
+    check_all_nodes
     !@answer.nil?
   end
 end
